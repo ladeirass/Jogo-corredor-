63caa23026db3ca56d290a6b2dfc627c39c0a955
ladeirass <ladeira@tutanota.com>
2023-12-11 11:52:38 -0300

    Botão de ação definidos, implementação da gravidade.

--- corredor.py
+++ corredor.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Corredor')
 relogio = pygame.time.Clock() #objeto relógio
 teste_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
 
 ceu_superf = pygame.image.load('graphics/Sky.png').convert()
 chao_superf = pygame.image.load('graphics/ground.png').convert()
@@ -22,7 +23,7 @@ caracol_retangulo = caracol_superf.get_rect(bottomright = (600,300 ) )
 
 jogador_superf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 jogador_retangulo = jogador_superf.get_rect(midleft = (80,260))   #criando um retângulo, definindo o ponto de contato
-
+jogador_gravidade = 0 #POR PADRÃO
 
     # desenha todos os nossos elementos 
     #atualização tudo
@@ -32,12 +33,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        #if event.type == pygame.MOUSEMOTION:
-            #if jogador_retangulo.collidepoint(event.pos): print('colisão') 
+       
+        if event.type == pygame.MOUSEBUTTONDOWN:#Verifica se algum botão do mouse esta pressionado
+            if jogador_retangulo.collidepoint(event.pos) and jogador_retangulo.bottom >=300:#a ação so aparece quando se clica em cima do personagem 
+                jogador_gravidade = -20  
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                print('pular')
+            if event.key == pygame.K_SPACE and jogador_retangulo.bottom >=300:
+                jogador_gravidade = -20 
     
     # blit significa tranfêrencia de imagem em bloco
     # foi passado dois argumentos o primeiro referente a tela e o outro a posição.)
@@ -51,30 +54,23 @@ while True:
     caracol_retangulo.x -= 4   
     if caracol_retangulo.right <= 0: caracol_retangulo.left = 800
     screen.blit(caracol_superf, caracol_retangulo)
-    
+
+    #Jogador
+    jogador_gravidade +=1
+    jogador_retangulo.y += jogador_gravidade #mapeia a gravidade atraves do retangulo do personagem
+    if jogador_retangulo.bottom >= 300: jogador_retangulo.bottom = 300
     screen.blit(jogador_superf, jogador_retangulo)
-   # pygame.draw.line(screen, 'gold', (0,0), pygame.mouse.get_pos(), 10)
     
-    #colisão de retangulos
-    # este método retorna um e zero
-    #if jogador_retangulo.colliderect(caracol_retangulo):
-     #   print('Colisão')
+    #Função para encerrar o jogo quando os personagens se colidem
+    if caracol_retangulo.colliderect(jogador_retangulo):
+        pygame.QUIT()
+        exit()
+
+    
     pygame.draw.rect(screen, '#c0e8ec', score_rect ) #modulo de desenho pygame
     pygame.draw.rect(screen, '#c0e8ec', score_rect, 20) #modulo de desenho pygame
     screen.blit(score_superf,score_rect)
-    '''teclado_acao = pygame.key.get_pressed()
-    if jogador_retangulo.collidepoint((teclado_acao)): # copiando a posição do mouse para a colisão 
-        print('Colisão')'''
-
-    # implementando uma lista especifica do pygame
-    '''keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]: 
-        print('jump')'''
-    
-    #mouse_pos = pygame.mouse.get_pos()
-   # if jogador_retangulo.collidepoint((mouse_pos)): # copiando a posição do mouse para a colisão 
-       # print(pygame.mouse.get_pressed())
-        
+   
     pygame.display.update()
     relogio.tick(60 ) #limita o jogo a 60fps
 
